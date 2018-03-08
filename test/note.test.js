2d86ab9f34fac5a2230a1485e2a94e5c244147d7
Chris Lemler <chris@lemler.org>
2018-03-08 08:41:00 -0800

    Cleadned up note test cases

--- test/note.test.js
+++ test/note.test.js
@@ -3,34 +3,8 @@ const uuid = require('uuid');
 const moment = require('moment');
 const util = require('util');
 
-// For example, let's say we had not just a city database, but also a food database. We could do different setup for different tests:
-// // Applies to all tests in this file
-// beforeEach(() => {
-//   return initializeCityDatabase();
-// });
-//
-// test('city database has Vienna', () => {
-//   expect(isCity('Vienna')).toBeTruthy();
-// });
-//
-// test('city database has San Juan', () => {
-//   expect(isCity('San Juan')).toBeTruthy();
-// });
-//
-// describe('matching cities to foods', () => {
-//   // Applies only to tests in this describe block
-//   beforeEach(() => {
-//     return initializeFoodDatabase();
-//   });
-//
-//   test('Vienna <3 sausage', () => {
-//     expect(isValidCityFoodPair('Vienna', 'Wiener Schnitzel')).toBe(true);
-//   });
-//
-//   test('San Juan <3 plantains', () => {
-//     expect(isValidCityFoodPair('San Juan', 'Mofongo')).toBe(true);
-//   });
-// });
+// Holds the test values for the Notes
+// created in the tests
 let content = '';
 let createdAt = '';
 let noteId = '';
@@ -43,13 +17,7 @@ describe('Can construct new Notes', () => {
   });
 
   test('Construct Note with correct parameters', () => {
-    let param = {};
-    param.content = content;
-    param.createdAt = createdAt;
-    param.noteId = noteId;
-    // @TODO look at ES6 object initialization to clean-up this code further
-    let note = new Note(param);
-    console.log(util.inspect(param));
+    let note = new Note({content, createdAt, noteId});
     console.log(util.inspect(note));
     expect(note.content).toEqual(content);
     expect(note.createdAt).toEqual(createdAt);
@@ -57,13 +25,7 @@ describe('Can construct new Notes', () => {
   });
 
   test('Construct Note without an ID', () => {
-    let param = {};
-    const content = 'This is a simple note.';
-    const createdAt = moment().toISOString();
-    param.content = content;
-    param.createdAt = createdAt;
-    let note = new Note(param);
-    console.log(util.inspect(param));
+    let note = new Note({content, createdAt});
     console.log(util.inspect(note));
     expect(note.content).toEqual(content);
     expect(note.createdAt).toEqual(createdAt);
@@ -71,13 +33,7 @@ describe('Can construct new Notes', () => {
   });
 
   test('Construct Note without a timestamp', () => {
-    let param = {};
-    const content = 'This is a simple note.';
-    const noteId = uuid.v4();
-    param.content = content;
-    param.noteId = noteId;
-    let note = new Note(param);
-    console.log(util.inspect(param));
+    let note = new Note({content, noteId});
     console.log(util.inspect(note));
     expect(note.content).toEqual(content);
     expect(note.noteId).toEqual(noteId);
@@ -85,12 +41,7 @@ describe('Can construct new Notes', () => {
   });
 
   test('Construct Note without an ID or timestamp', () => {
-    let param = {};
-    const content = 'This is a simple note.';
-    const createdAt = moment().toISOString();
-    param.content = content;
-    let note = new Note(param);
-    console.log(util.inspect(param));
+    let note = new Note({content});
     console.log(util.inspect(note));
     expect(note.content).toEqual(content);
     expect(note.createdAt).not.toBeUndefined();

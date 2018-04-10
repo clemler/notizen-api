b053a0930fcae1f40c69e17352af607aa65b1e12
Chris Lemler <chris@lemler.org>
2018-04-09 20:34:35 -0700

    Updating Create to call the underlaying notiz functions

--- create.js
+++ create.js
@@ -1,36 +1,27 @@
-import uuid from 'uuid';
-import AWS from 'aws-sdk';
-import Logger from "./lib/logger";
-const logger = new Logger("notizen-api");
+//const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const Logger = require('./lib/logger');
+const util = require('util');
+const { createNote, validateNote, saveNote } = require('./lib/notiz.js');
+
+const logger = new Logger('notizen-api');
 logger.info('I am here');
 
 AWS.config.update({region: 'us-west-2'});
 const db = new AWS.DynamoDB.DocumentClient();
-// @TODO: This should be passed via an environment variable in serverless.yml
-const TABLE_NAME = 'notizen-api-table-dev';
 
-export function main(event, context, callback) {
+function main(event, context, callback) {
   // Request body is passed in as a JSON encoded string in
   // 'event.body'
   const data = JSON.parse(event.body);
-
-  const params = {
-    TableName: TABLE_NAME,
-    // 'Item' contains the attributes of the item to be created
-    // - 'userId': user identities are federated through the
-    //             Cognito Identity Pool, we will use the identity id
-    //             as the user id of the authenticated user
-    // - 'noteId': a unique uuid
-    // - 'content': parsed from request body
-    // - 'attachment': parsed from request body
-    // - 'createdAt': current Unix timestamp
-    Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: new Date().getTime()
-    }
-  };
-
+  logger.info(`Entered Create() --> ${util.inspect(data)}`);
+  const NOTIZEN_TABLE = process.env.NOTIZEN_TABLE;
+  logger.info(`Dynamo Table --> ${NOTIZEN_TABLE}`);
+  createNote('USER-1234', data.content, data.attachment)
+    .then((note) => {
+      logger.info(util.inspect(note));
+    });
+  callback(null, data);
 }
+
+module.exports = { main };
